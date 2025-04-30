2e6d4aba0d8b59d753b8f970b121b9d64b5da364
Cordova Anthony <72144025+18-anth@users.noreply.github.com>
2025-04-29 22:58:25 -0500

    add archives

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "619611156"
+    serviceWorkerVersion: "3963753685"
   }
 });
