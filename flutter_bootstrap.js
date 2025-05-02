7c9ad6c01d7368623c580470cf88280eee165a00
Cordova Anthony <72144025+18-anth@users.noreply.github.com>
2025-05-02 15:53:07 -0500

    add screens and fonts

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1341902052"
+    serviceWorkerVersion: "1694123581"
   }
 });
